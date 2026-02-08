b96c197cdacb7d0a4f5a56f7645516ede3e6851e
Xinya Chen <chen.xinya1@northeastern.edu>
2026-02-08 15:29:07 -0800

    Configure cors for vercel frontend

--- Django/config/settings.py
+++ Django/config/settings.py
@@ -136,3 +136,8 @@ STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://your-project-name.vercel.app"
+]
+
