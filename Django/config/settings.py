3ec713dafb0194ee805180c7f16281089c31bc7b
Xinya Chen <chen.xinya1@northeastern.edu>
2026-02-08 11:13:26 -0800

    Configure Debug and allow_hosts for render

--- Django/config/settings.py
+++ Django/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&cimntwx=fhjpx0@k8((gs!0o2hnf40#brq^69v(pgww5md2uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
