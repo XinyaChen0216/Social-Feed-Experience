94c27023c86cb3d7e44f4f9824fd5dc18c7599ec
Xinya Chen <chen.xinya1@northeastern.edu>
2026-02-08 11:49:04 -0800

    Configure env variables for render

--- Django/config/settings.py
+++ Django/config/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&cimntwx=fhjpx0@k8((gs!0o2hnf40#brq^69v(pgww5md2uw'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"
-
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = ["*"]
 
 
